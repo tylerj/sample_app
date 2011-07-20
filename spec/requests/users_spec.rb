be60e77be3398c057f65971620d5db4556aad80e
Tyler Johnson <tylerl.johnson@gmail.com>
2011-07-20 00:17:58 -0400

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe "Users" do
+
   describe "signup" do
+    
     describe "failure" do
+      
       it "should not make a new user" do
         lambda do
           visit signup_path
@@ -18,6 +21,7 @@ describe "Users" do
     end
 
     describe "success" do
+    
       it "should make a new user" do
         lambda do
           visit signup_path
@@ -32,5 +36,27 @@ describe "Users" do
         end.should change(User, :count).by(1)
       end
     end
-  end  
+  end
+  
+  describe "sign in/out" do
+  
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email, :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+    
+    describe "success" do
+      it "should sign a user in and out" do
+        integration_sign_in(Factory(:user))
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
